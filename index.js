66addfacbcb738d178bc2ffdb379b32dffb2a979
lokeslokesh12 <lokesh272203@gmail.com>
2022-11-20 18:05:20 +0530

    plan api

---
+++ index.js
@@ -0,0 +1,81 @@
+let express = require('express');
+let cors = require('cors');
+let dotenv = require('dotenv');
+dotenv.config();
+let app = express();
+let port = process.env.PORT || 5000
+let Mongo = require('mongodb');
+const { query } = require('express');
+let MongoClient = Mongo.MongoClient;
+let mongouturl = process.env.LiveMongo;
+let bodyparser = require('body-parser')
+let db;
+
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.json())
+
+app.use(cors());
+
+// 
+app.get('/',(req,res)=>{
+    db.collection('Allpacks').find().toArray((err,result)=>{
+        if(err) {
+            console.log(err , "err while listing");
+            res.send("Error while connecting to  server :-(")
+        }
+        res.send("Every thing is fine :-)")
+    })
+} 
+)
+
+// list all data in database which is connected---------------------------------
+
+app.get('/plans',(req,res)=>{
+    db.collection('Allpacks').find().toArray((err,result)=>{
+        if(err) console.log(err , "err while listing");
+        res.send(result)
+    })
+} 
+)
+
+
+//filter type respect to cost---------------------------------
+
+app.get('/filter/:type',(req,res)=>{
+    let type = req.params.type
+    let cost = Number(req.query.cost)
+    var query ={}
+    if(cost){
+        query={"type":type,"cost": cost }
+    }
+    else if(type){
+        query={"type":type }
+    }
+    else{
+        query={}
+    }
+    db.collection('Allpacks').find(query).toArray((err,result)=>{
+        if(err) console.log(err);
+        res.send(result)
+    })
+})
+
+
+
+// connect with database---------------------------------
+
+MongoClient.connect(mongouturl,(err,client)=>{
+    if(err) console.log('err while connect');
+    db = client.db('Airtel');
+  
+    app.listen(port,()=>{
+        console.log('servre is runnun in ' + port )
+    })
+    
+})
+
+
+
+
+
+
